72e582636fadcf7460dfa5275a6d8b644671e035
Takumi Maruyama <satouredstone@gmail.com>
2022-02-19 15:00:14 +0000

    Finish overview/middleware section

---
+++ src/logger.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Request...');
+    next();
+  }
+}
+
+export function loggerFunctional(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  console.log('Request...(functional logger)');
+  next();
+}
